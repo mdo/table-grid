2ac2c6d803f99513307bcde5cd8de02967bd5ea7
Jaclyn Tan <jaclyntan7@gmail.com>
2016-12-10 01:28:37 +0800

    Major overhaul of the CSS and structure
    This update includes:
    - Major overhaul and reformatting of the grid system using sass.
    - Added a package.json and gulp file
    - Added a new pdate.html file in the layouts folder explaining the
    changes.
    
    Disclaimer: I am really new to Github and Markdown so the doc is just
    in html for now.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        proxy: "localhost:8888/table-grid/layouts/update.html"
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+
+gulp.task('styles', function () {
+    gulp.src(['sass/**/*.scss'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload())
+});
+
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("sass/**/*.scss", ['styles']);
+    gulp.watch("layouts/**/*.html", ['bs-reload']);
+});
